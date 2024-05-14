c6e4155ecf87c8656162cd29afd7f6e2164e6c86
user664635 <myy12358@gmail.com>
2024-05-14 23:15:27 +0800

    keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,10 @@
-vim.keymap.set('n', '<A-h>', '<C-w>h')
-vim.keymap.set('n', '<A-j>', '<C-w>j')
-vim.keymap.set('n', '<A-k>', '<C-w>k')
-vim.keymap.set('n', '<A-l>', '<C-w>l')
+local kmp = vim.keymap
+kmp.set('n', '<A-h>', '<C-w>h')
+kmp.set('n', '<A-j>', '<C-w>j')
+kmp.set('n', '<A-k>', '<C-w>k')
+kmp.set('n', '<A-l>', '<C-w>l')
 
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+kmp.set('v', '<', '<gv')
+kmp.set('v', '>', '>gv')
+kmp.set('v', '<C-S-A>', '<esc>O/*<esc>gvo<esc>o*/<esc>')
+kmp.set('v', '<C-S-C>', '<esc>ddgvo<esc>dd')
