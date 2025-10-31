6ca15a1b2674609315b6ebfebecf3086e0fe1a60
user664635 <myy12358@gmail.com>
2025-10-31 18:24:53 +0800

    lsp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -111,11 +111,5 @@ return {
 			}
 		)
 
-		-- Set up lspconfig.
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-		require("lspconfig")["clangd"].setup {
-			capabilities = capabilities
-		}
 	end
 }
